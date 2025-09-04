daca05e86c786c33729afe1117e3b2aace89868e
3epa <egorfelix@yandex.ru>
2025-09-04 15:41:00 +0300

    add to all functionDTO correct exact function

--- src/main/java/com/itmo/Main.java
+++ src/main/java/com/itmo/Main.java
@@ -16,24 +16,24 @@ public class Main {
         functions.add(new FunctionDTO(
                 "y + (1 + x) * y^2",
                 (x, y) -> y + (1 + x) * Math.pow(y, 2),
-                (x, x0, y0) -> -Math.exp(x) / (x*Math.exp(x)+Math.exp(x0)*(-1/y0-x0)))
+                (x, x0, y0) -> -Math.exp(x) / (x * Math.exp(x) + Math.exp(x0) * (-1 / y0 - x0)))
         );
-//        functions.add(new FunctionDTO(
-//                "2x - y",
-//                (x, y) -> 2 * x + y,
-//                x -> 3 * Math.exp(-x) + 2 * x - 2)
-//        );
-//        functions.add(new FunctionDTO(
-//                "y / (1 + x^2)",
-//                (x, y) -> y / (1 + Math.pow(x, 2)),
-//                x -> Math.exp(Math.atan(x)))
-//        );
-//
-//        functions.add(new FunctionDTO(
-//                "3x^2",
-//                (x, y) -> 3*Math.pow(x,2),
-//                x -> Math.pow(x,3))
-//        );
+        functions.add(new FunctionDTO(
+                "2x - y",
+                (x, y) -> 2 * x - y,
+                (x, x0, y0) -> Math.exp(x0) * (y0 - 2 * x0 + 2) * Math.exp(-x) + 2 * x - 2)
+        );
+        functions.add(new FunctionDTO(
+                "y / (1 + x^2)",
+                (x, y) -> y / (1 + Math.pow(x, 2)),
+                (x, x0, y0) -> y0 / Math.exp(Math.atan(x0)) * Math.exp(Math.atan(x)))
+        );
+
+        functions.add(new FunctionDTO(
+                "3x^2",
+                (x, y) -> 3 * Math.pow(x, 2),
+                (x, x0, y0) -> Math.pow(x, 3) + (y0 - Math.pow(x0, 3)))
+        );
 
         List<AbstractODESolver> odeSolvers = new ArrayList<>();
         odeSolvers.add(new ImprovedEulerMethod());
